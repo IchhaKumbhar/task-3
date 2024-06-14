020689475376963c928978f9e56a751c3c451ec7
IchhaKumbhar <146089024+IchhaKumbhar@users.noreply.github.com>
2024-06-14 14:48:53 +0530

    Add files via upload

---
+++ src/pages/SourceFile1.jsx
@@ -0,0 +1,125 @@
+export default function SourceFile1() {
+  return (
+    <div className="bg-[#171047] flex flex-row p-[84px_120px_84px_0] w-[1440px] box-sizing-border">
+      <img className="rotate-[145.196deg] absolute left-[-495px] top-[-413.5px] w-[2090.8px] h-[1760.3px]" />
+      <div className="shadow-[0px_4px_4px_0px_rgba(0,0,0,0.38)] relative m-[59px_82px_58px_0] flex p-[597.1px_0_0_0] w-[1149px] h-[615px] box-sizing-border">
+        <div className="opacity-60 mix-blend-multiply bg-[url('assets/images/Shadow2.png')] relative w-[1149px] h-[17.9px]">
+        </div>
+        <div className="bg-[url('assets/images/Color.png')] absolute left-[53.6px] top-[0px] right-[49.4px] h-[606.2px]">
+        </div>
+      </div>
+      <div className="relative flex flex-col items-center box-sizing-border">
+        <div className="m-[0_2.7px_32px_1px] flex flex-col w-[fit-content] box-sizing-border">
+          <p className="m-[0_0_10px_0] self-start break-words font-['Poppins'] font-semibold text-[24px] leading-[1.083] text-[#FFFFFF]">
+          <span className="login-in-to-podcesin-sub-17"></span><span className="login-in-to-podcesin-sub-0"></span><span className="login-in-to-podcesin-sub-17"></span><span></span>
+          </p>
+          <span className="break-words font-['Poppins'] font-normal text-[14px] leading-[1.714] text-[#FFFFFF]">
+          You can search course, apply course and find the course or scholarship student for abroad studies around the world
+          </span>
+        </div>
+        <div className="flex flex-col items-center w-[fit-content] box-sizing-border">
+          <div className="m-[0_0_32px_1px] flex flex-col items-center w-[fit-content] box-sizing-border">
+            <div className="rounded-[4px] m-[0_0_24px_0] flex flex-row w-[400px] box-sizing-border">
+              <div className="rounded-[4px] bg-[#FFFFFF] m-[0_16px_0_0] flex flex-row p-[16px_0_16px_0] w-[192px] box-sizing-border">
+                <div className="m-[2px_12px_2px_0] flex w-[20px] h-[20px] box-sizing-border">
+                  <img className="w-[20px] h-[20px]" />
+                </div>
+                <span className="break-words font-['Poppins'] font-normal text-[16px] text-[#888888]">
+                Google
+                </span>
+              </div>
+              <div className="rounded-[4px] bg-[#FFFFFF] flex flex-row p-[16px_0_16px_0] w-[192px] box-sizing-border">
+                <div className="m-[2px_12px_2px_0] flex w-[20px] h-[20px] box-sizing-border">
+                  <img className="w-[20px] h-[20px]" />
+                </div>
+                <span className="break-words font-['Poppins'] font-normal text-[16px] text-[#888888]">
+                Facebook
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row w-[fit-content] box-sizing-border">
+              <div className="bg-[#F2F2F2] m-[9px_8px_8px_0] w-[184px] h-[1px]">
+              </div>
+              <span className="m-[0_8.8px_0_0] break-words font-['Poppins'] font-normal text-[14px] leading-[1.286] text-[#888888]">
+              Or
+              </span>
+              <div className="bg-[#F2F2F2] m-[9px_0_8px_0] w-[183px] h-[1px]">
+              </div>
+            </div>
+          </div>
+          <div className="m-[0_0_40px_0] flex flex-col w-[fit-content] box-sizing-border">
+            <div className="m-[0_0_24px_1px] flex flex-col w-[fit-content] box-sizing-border">
+              <div className="opacity-50 m-[0_2px_12px_2px] inline-block self-start break-words font-['Poppins'] font-normal text-[12px] text-[#FFFFFF]">
+              Full Name
+              </div>
+              <div className="opacity-20 rounded-[4px] border-[1px_solid_#FFFFFF] bg-[#D9D9D9] relative flex flex-row p-[14px_0_14px_16px] w-[400px] box-sizing-border">
+                <span className="m-[0_2.7px_0_0] break-words font-['Poppins'] font-normal text-[14px] leading-[1.429] text-[#FFFFFF]">
+                Alexander Michael
+                </span>
+                <div className="bg-[#FFFFFF] w-[1px] h-[20px]">
+                </div>
+              </div>
+            </div>
+            <div className="rounded-[4px] relative m-[0_0_24px_1px] p-[0_2px_14px_2px] w-[fit-content] box-sizing-border">
+              <div className="opacity-[0.22] rounded-[4px] border-[1px_solid_#FFFFFF] bg-[#D9D9D9] absolute left-[0px] right-[0px] bottom-[0px] h-[48px]">
+              </div>
+              <div className="relative flex flex-col box-sizing-border">
+                <div className="opacity-50 m-[0_0_26px_0] inline-block self-start break-words font-['Poppins'] font-normal text-[12px] text-[#FFFFFF]">
+                Email Address
+                </div>
+                <span className="m-[0_0_0_14px] break-words font-['Poppins'] font-normal text-[14px] leading-[1.429] text-[#FFFFFF]">
+                alexmichael@gmail.com
+                </span>
+              </div>
+            </div>
+            <div className="relative m-[0_0_24px_1px] p-[0_2px_13px_2px] w-[fit-content] box-sizing-border">
+              <div className="opacity-[0.22] rounded-[4px] border-[1px_solid_#FFFFFF] bg-[#D9D9D9] absolute left-[0px] right-[0px] bottom-[0px] h-[48px]">
+              </div>
+              <div className="relative flex flex-col box-sizing-border">
+                <div className="opacity-50 m-[0_0_25px_0] inline-block self-start break-words font-['Poppins'] font-normal text-[12px] text-[#FFFFFF]">
+                Password
+                </div>
+                <span className="m-[0_0_0_14px] break-words font-['Poppins'] font-normal text-[20px] leading-[1.1] text-[#FFFFFF]">
+                •••••••••••••
+                </span>
+              </div>
+              <div className="absolute right-[22.5px] bottom-[20.4px] flex w-[6.8px] h-[6.8px] box-sizing-border">
+                <img className="w-[6.8px] h-[6.8px]" />
+              </div>
+            </div>
+            <div className="m-[0_0.3px_0_0] flex flex-row justify-between w-[400.7px] box-sizing-border">
+              <div className="flex flex-row box-sizing-border">
+                <div className="opacity-[0.88] rounded-[4px] bg-[#3E10E3] relative m-[0_8px_0_0] flex p-[7px_6px_7px_6px] w-[20px] h-[20px] box-sizing-border">
+                  <div className="flex w-[8px] h-[6px] box-sizing-border">
+                    <img className="w-[8px] h-[6px]" />
+                  </div>
+                </div>
+                <div className="m-[1px_0_1px_0] inline-block break-words font-['Poppins'] font-normal text-[12px] text-[#FFFFFF]">
+                Remember me
+                </div>
+              </div>
+              <div className="m-[1px_0_1px_0] inline-block break-words font-['Poppins'] font-medium text-[12px] underline text-[#FFFFFF]">
+              Forgot Password?
+              </div>
+            </div>
+          </div>
+          <div className="rounded-[4px] m-[0_2px_0_1px] flex flex-col items-center w-[398px] box-sizing-border">
+            <div className="rounded-[4px] bg-[#F26767] m-[0_0_16px_0] flex p-[16px_0_16px_0] w-[398px] box-sizing-border">
+              <span className="break-words font-['Poppins'] font-semibold text-[16px] text-[#FFFFFF]">
+              Login
+              </span>
+            </div>
+            <div className="m-[0_0_0_2.5px] flex flex-row w-[fit-content] box-sizing-border">
+              <span className="m-[0_8.2px_0_0] break-words font-['Poppins'] font-normal text-[12px] text-[#FFFFFF]">
+              Don’t have an account ?
+              </span>
+              <span className="break-words font-['Poppins'] font-semibold text-[12px] text-[#3E10E3]">
+              Register here
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
